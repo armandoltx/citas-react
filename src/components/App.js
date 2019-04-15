0710027a8d1290bfe44d464e1f8ea621a06228b2
armandoltx <armando@armandoltx.com>
2019-04-15 12:18:35 +1000

    Creado el componente AgregarCita

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import AgregarCita from './AgregarCita';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header titulo={"Administrador de Pacientes de Veterinaria"} />
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita />
+          </div>
+        </div>
       </div>
     );
   }
