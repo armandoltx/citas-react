a0f3b231b00f6b7280772f20cdc91cdf73082101
armandoltx <armando@armandoltx.com>
2019-04-15 12:06:17 +1000

    Creando functional component header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="container">
+        <Header titulo={"Administrador de Pacientes de Veterinaria"} />
       </div>
     );
   }
