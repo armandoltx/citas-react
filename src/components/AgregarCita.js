b1c326845333398bfdb01691573722098695e676
armandoltx <armando@armandoltx.com>
2019-04-15 12:41:59 +1000

    Leyendo los valores del formulario

--- src/components/AgregarCita.js
+++ src/components/AgregarCita.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class AgregarCita extends Component {
 
+  // refs para los campos del formulario
+  nombreMascotaRef = React.createRef();
+  propietarioRef = React.createRef();
+  fechaRef = React.createRef();
+  horaRef = React.createRef();
+  sintomasRef = React.createRef();
 
   //crear onSubmit funcion para el formulario
   crearNuevaCita = (e) => {
     // 1. Prevenir default para q no se recargue
       e.preventDefault();
       console.log("Dentro de crearNuevaCita");
+      console.log("nombreMascotaRef ", this.nombreMascotaRef.current.value);
+      console.log("propietarioRef ", this.propietarioRef.current.value);
+      console.log("fechaRef ", this.fechaRef.current.value);
+      console.log("horaRef ", this.horaRef.current.value);
+      console.log("sintomasRef ", this.sintomasRef.current.value);
+
 
     // 2. Crear el Objeto con los datos
 
@@ -27,32 +39,32 @@ class AgregarCita extends Component {
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
               <div className="col-sm-8 col-lg-10">
-                <input type="text" className="form-control" placeholder="Nombre Mascota" />
+                <input ref={this.nombreMascotaRef} type="text" className="form-control" placeholder="Nombre Mascota" />
               </div>
             </div>
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Nombre Dueño</label>
               <div className="col-sm-8 col-lg-10">
-                <input type="text" className="form-control" placeholder="Nombre Dueño de la Mascota" />
+                <input ref={this.propietarioRef} type="text" className="form-control" placeholder="Nombre Dueño de la Mascota" />
               </div>
             </div>
 
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
               <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
-                <input type="date" className="form-control" />
+                <input ref={this.fechaRef} type="date" className="form-control" />
               </div>
 
               <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
               <div className="col-sm-8 col-lg-4">
-                <input type="time" className="form-control" />
+                <input ref={this.horaRef} type="time" className="form-control" />
               </div>
             </div>
 
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
               <div className="col-sm-8 col-lg-10">
-                <textarea className="form-control"></textarea>
+                <textarea ref={this.sintomasRef} className="form-control"></textarea>
               </div>
             </div>
             <div className="form-group row justify-content-end">
